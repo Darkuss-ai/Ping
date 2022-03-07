efd062ad16479478795f35bd0a862c99c071716a
Sergey <67109983+Darkuss-ai@users.noreply.github.com>
2022-03-07 12:00:40 +0300

    Add files via upload

--- loc_server-test/src/main.rs
+++ loc_server-test/src/main.rs
@@ -3,39 +3,51 @@ use std::time;
 use std::net::{TcpListener,TcpStream};
 use std::io::{Read,Write};
 use std::thread;
+use loc_server_test::ThreadPool;
 fn main() {
     let receiver_listener = TcpListener::bind("127.0.0.1:7878").expect("Failed and bind with the sender");
     // Getting a handle of the underlying thread.
-    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+    let size = 4;
+    let pool = ThreadPool::new(size);
+    let mut con = false;
+
     // listen to incoming connections messages and bind them to a sever socket address.
     for stream in receiver_listener.incoming() {
-        let stream = stream.expect("failed");
-        // let the receiver connect with the sender
-        let handle = thread::spawn(move || {
-            //receiver failed to read from the stream
-            handle_sender(stream).unwrap_or_else(|error| eprintln!("{:?}",error))
-        });
-        
-        // Push messages in the order they are sent
-        thread_vec.push(handle);
-    }
+        if con == false{
+            con = true;
+            let stream = stream.expect("failed");
 
-    for handle in thread_vec {
-        // return each single value Output contained in the heap
-        handle.join().unwrap();
+            pool.execute(|| {
+                handle_sender(stream, 1);
+            });
+        }
+
+        else {
+            con = false;
+            let stream = stream.expect("failed");
+
+            pool.execute(|| {
+                handle_sender(stream, 2);
+            });
+        }
+        // let the receiver connect with the sender
+        // Можно вместо bool сделать тупо i64 и увеличивать её каждый раз
+        //Когда кто-то подключается, но я тестировал на 2 клиентах
+        
+        
+        println!("Shutting down.");
     }
-    // success value
 }
 
-fn handle_sender(mut stream: TcpStream) -> io::Result<()>{
-    let mut buf = [0;512];
+fn handle_sender(mut stream: TcpStream, mut id: u8) -> io::Result<()>{
+    let mut buf = [0;1024];
     for _ in 0..1000{
         let bytes_read = stream.read(&mut buf)?;
         if bytes_read == 0{
             return Ok(());
         }
         stream.write(&buf[..bytes_read])?;
-        println!("from the sender:{}",String::from_utf8_lossy(&buf));
+        println!("from the sender{}:{}", id, String::from_utf8_lossy(&buf));
         thread::sleep(time::Duration::from_secs(1)); 
     }
     Ok(())
